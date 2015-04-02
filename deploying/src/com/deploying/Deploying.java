0a4547c395e419ff721c998dd64eda7c5c9b91e9
Ganya <ganyacp@gmail.com>
2015-04-02 14:40:29 +0530

    commit the deploying

---
+++ deploying/src/com/deploying/Deploying.java
@@ -0,0 +1,14 @@
+package com.deploying;
+
+public class Deploying {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Executing war");
+
+	}
+
+}
